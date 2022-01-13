3cdb7fae20b2299d5ef1d3fcff621c0c05e99f68
maurice chisom <mauricechisom275@gmail.com>
2022-01-13 11:57:17 +0100

    converted temp (celsius to kelvin)

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,7 +6,7 @@ export default function Dislay({wth}) {
         <div className='display opacity'>
             <h3>{wth.name}<sup className='country'><small>{wth.sys.country}</small></sup></h3>
             <br />
-            <h1>{Math.round(wth.main.temp)}<sup><small>&deg;C</small></sup></h1>
+            <h1>{Math.round(wth.main.temp) - 273}<sup><small>&deg;C</small></sup></h1>
             <br />
             <div className='info'>
                 <img className='city-icon' src={`https://openweathermap.org/img/wn/${wth.weather[0].icon}@2x.png`} alt={wth.weather[0].description} />
